e4ed22cf156fa2375c28bc59029b7f55abc8e28e
fitz <fitz@gmail.com>
2024-09-27 01:31:47 +0800

    can push image to my.io

--- README.md
+++ README.md
@@ -57,7 +57,7 @@ docker compose up -d
 *以自建仓库ip为192.168.1.1为例，修改/etc/hosts 添加以下内容*  
 
 ```sh
-192.168.1.1 gcr.io quay.io docker.io registry-1.docker.io nvcr.io registry.k8s.io custom.local
+192.168.1.1 gcr.io quay.io docker.io registry-1.docker.io nvcr.io registry.k8s.io custom.local my.io
 ```
 
 ### 信任证书
@@ -84,11 +84,20 @@ sudo cp cert/ca.crt /etc/pki/ca-trust/source/anchors/
 sudo update-ca-trust
 ```
 
+### 如果想支持push自己的image到my.io，需要修改docker配置文件
+vim /etc/docker/daemon.json
+```
+{
+	"registry-mirrors": ["https://my.io"],
+	"insecure-registries": ["my.io"]
+}
+```
+
 ### 重启docker
 
 ```sh
-sudo systemctl daemon-reload
-sudo systemctl restart docker
+systemctl daemon-reload
+systemctl restart docker
 ```
 
 ### 测试
@@ -106,4 +115,30 @@ docker pull gcr.io/google-containers/pause:3.2
 docker pull ghcr.io/coder/coder:v2.13.0
 # nvcr.io
 docker pull nvcr.io/nvidia/k8s/cuda-sample:devicequery
+```
+
+### 测试my.io
+```sh
+docker pull hello-world
+docker tag hello-world my.io/hello
+docker push my.io/hello
+curl -k  https://my.io/v2/_catalog
+docker rmi my.io/hello
+docker pull my.io/hello
+```
+
+### 如何删除my.io中的镜像
+- [有文章提到](https://github.com/burnettk/delete-docker-registry-image)
+- [文章2，看简易版部分](https://blog.csdn.net/u014756339/article/details/121289329)
+    - curl -k  https://127.0.0.1/v2/_catalog
+    - 删掉想删掉的镜像
+    - （推荐）去 registry-mirror/data/docker/registry/v2/repositories 找到镜像并删除
+    - 或者
+    - （不推荐）docker exec custom  rm -rf /var/lib/registry/docker/registry/v2/repositories/hello1
+    - （貌似不做也行) docker exec custom  /bin/registry garbage-collect /etc/docker/registry/config.yml
+    - curl -k  https://127.0.0.1/v2/_catalog
+
+```
+
+
 ```
--- gencert.sh
+++ gencert.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# 检查 OpenSSL 命令是否存在
-if ! command -v openssl &>/dev/null; then
-    echo "错误：未安装 OpenSSL 或未将其添加到 PATH 中。"
-    exit 1
-fi
 
-read -s -p "设置 CA 密码：" password
+password=1234
+# read -p "设置 CA 密码：" password
 echo ""
 
 # 创建 cert 目录
@@ -14,14 +10,13 @@ cert_dir="cert"
 mkdir -p $cert_dir
 
 # 证书的基本信息
-days=3650
+days=36500
 country="CN"
 state="Shanghai"
 locality="Shanghai"
 organization="registry"
 organizational_unit="registry"
 common_name="registry.com"
-alt_names="DNS:gcr.io,DNS:ghcr.io,DNS:*.k8s.io,DNS:*.docker.io,DNS:quay.io,DNS:nvcr.io,DNS:custom.local"
 
 # 生成自签名的 CA 私钥和证书
 generate_ca() {
@@ -30,23 +25,32 @@ generate_ca() {
     openssl req -x509 -new -key $cert_dir/ca.key -out $cert_dir/ca.crt -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizational_unit/CN=$common_name" -days $days -passin pass:"$password"
 }
 
-# 生成服务器证书签署请求（CSR）和私钥
-generate_server_csr() {
-    echo "正在生成服务器证书签署请求（CSR）和私钥..."
-    openssl req -new -keyout $cert_dir/server.key -out $cert_dir/server.csr -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizational_unit/CN=$common_name" -nodes
+# 生成服务器私钥
+gen_server_rsa() {
+    echo "生成服务器私钥..."
+    openssl genrsa -out $cert_dir/server.key 4096
 }
+
+# 生成服务器证书签署请求
+gen_server_csr() {
+    echo "生成服务器证书签名请求..."
+    openssl req -new -key $cert_dir/server.key -out $cert_dir/server.csr  -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizational_unit/CN=$common_name"
+}
+
 
 # 使用 CA 对 CSR 进行签名，生成服务器证书
 sign_server_certificate() {
     echo "正在使用 CA 对 CSR 进行签名，生成服务器证书..."
-    openssl x509 -req -in $cert_dir/server.csr -CA $cert_dir/ca.crt -CAkey $cert_dir/ca.key -CAcreateserial -out $cert_dir/server.crt -days $days -extfile <(echo "subjectAltName=$alt_names") -passin pass:"$password"
+    openssl x509 -req -in $cert_dir/server.csr -CA $cert_dir/ca.crt -CAkey $cert_dir/ca.key -CAcreateserial -out $cert_dir/server.crt -days $days -passin pass:"$password" -extfile extfile.txt
 }
 
 # 执行证书生成流程
 main() {
-    generate_ca
-    generate_server_csr
-    sign_server_certificate
+    # generate_ca
+    # gen_server_rsa
+    # gen_server_csr
+    # sign_server_certificate
+    echo "please modify gencert.sh to execute the logic you want\n"
 }
 
 main
